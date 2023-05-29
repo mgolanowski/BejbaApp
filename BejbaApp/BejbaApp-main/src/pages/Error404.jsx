1c575dc3ff9737431eb283f02e7639d5c8eb6929
mgolanowski <golanowskiwsb@gmail.com>
2023-05-29 21:39:21 +0200

    Navigation and globalReducer have been changed

--- BejbaApp/BejbaApp-main/src/pages/Error404.jsx
+++ BejbaApp/BejbaApp-main/src/pages/Error404.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error404 () {
     return (
     <div className="error">
-        <h1> Its Kajna Krejza Bejba bo ta strona nie iestnieje, jak się tu znalazłeś?</h1>
+        <h1> Its Kajna Krejza Bejba bo ta strona nie istnieje, jak się tu znalazłeś?</h1>
         <img src ="https://i.pinimg.com/originals/a5/4b/f8/a54bf8e8bd76d92be03bbecae09c1b69.png" width="70%" height="80%" />
     </div>
     );
